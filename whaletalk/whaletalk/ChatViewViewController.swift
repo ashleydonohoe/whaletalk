f92c7a3124f847be36ef4576fd3eff0135dc2a8e
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-04-09 23:13:17 -0400

    Have ChatViewController and custom cell for tableview set up

---
+++ whaletalk/whaletalk/ChatViewViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ChatViewController.swift
+//  whaletalk
+//
+//  Created by Gabriele on 4/9/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    
+    private var messages = [Message]()
+    private let cellIdentifier = "Cell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...10 {
+            let m = Message()
+            m.text = String(i)
+            messages.append(m)
+        }
+        
+        tableView.registerClass(ChatCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        let tableViewConstraints: [NSLayoutConstraint] = [
+            tableView.topAnchor.constraintEqualToAnchor(view.topAnchor),
+            tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+            tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+            tableView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activateConstraints(tableViewConstraints)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ChatCell
+        let message = messages[indexPath.row]
+        cell.messageLabel.text = message.text
+        return cell
+    }
+}
---
+++ whaletalk/whaletalk/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  whaletalk
+//
+//  Created by Gabriele on 4/9/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    var text: String?
+}
